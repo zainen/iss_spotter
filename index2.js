cdab4d729932bd74320d6a61cb3b4d9a01b4fada
zainen <zainensuzuki@gmail.com>
2020-12-04 05:25:38 +0000

    forgot to save one

--- index2.js
+++ index2.js
@@ -27,24 +27,3 @@ nextISSTimesForMyLocation()
     printPassTimes(passTimes);
   })
 
-
-
-
-// const { fetchCoordsByIP } = require('./iss_promised')
-// const { fetchISSFlyOverTimes} = require('./iss_promised')
-
-// const { PassThrough } = require('stream')
-// const { nextISSTimesForMyLocation } = require('./iss_promised')
-
-// fetchMyIP()
-//   .then(fetchCoordsByIP)
-//   .then(fetchISSFlyOverTimes)
-//   .then(body => console.log(body))
-
-// // nextISSTimesForMyLocation()
-//   // .then((passTimes) => {
-//   //   printPassTimes(passTimes)
-//   // })
-//   // .catch((error) => {
-//   //   console.log('It didn\'t work: ', error.message)
-//   // });
